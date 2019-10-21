0c5e0a5e2fd2335762d825943068baeba95a3031
kkk987 <markying21549@gmail.com>
2019-10-21 15:05:16 +1000

    setup environment including mongoose

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const port = 3000;
+const dbConn = 'mongodb://localhost/my_store';
+
+const app = express();
+
+mongoose.connect(dbConn, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  },
+  (err) => {
+    if (err) {
+      console.log('Error connecting to database', err);
+    }else {
+      console.log('Connected to database!');
+    }
+  }
+);
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`Blog app listening on port ${port}`);
+})
