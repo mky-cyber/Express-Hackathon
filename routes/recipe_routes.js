92e5cf518200b7759b24b7744daeae34e8ff86de
Tony Huynh <huynh.tony55@gmail.com>
2019-10-22 14:49:06 +1000

    implemented routing

--- routes/recipe_routes.js
+++ routes/recipe_routes.js
@@ -1,1 +1,25 @@
-server.set('view engine', 'ejs');
+const express = require('express');
+const router = express.Router();
+const {
+    getRecipes,
+    makeRecipe,
+    removeRecipe,
+    changeRecipe,
+    getRecipe
+} = require("../controllers/recipe_controllers");
+
+// CREATE
+router.post("/", makeRecipe);
+
+// READ
+router.get("/", getRecipes);
+
+router.get("/:id", getRecipe);
+
+// DELETE
+router.delete("/:id", removeRecipe);
+
+// UPDATE
+router.put("/:id", changeRecipe)
+
+module.exports = router;
