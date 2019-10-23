1299807a0fe74d390e3a818b66cbaa884437f617
kkk987 <markying21549@gmail.com>
2019-10-23 15:51:46 +1000

    Updates: - Modified getRecipe functionality - client can get all recipe names and a random recipe

--- views/pages/script.js
+++ views/pages/script.js
@@ -1,8 +1,10 @@
-let serverUrl = "http://localhost:5000/";
-let recipesUrl = serverUrl
+const serverUrl = "http://localhost:5000/";
+const recipesUrl = serverUrl;
+const recipeUrl =  serverUrl + 'random';
 
 function displayRecipes(recipes) {
     let section = document.querySelector("#recipe-list")
+    section.innerHTML = null;
     let ul = document.createElement("ul")
     section.appendChild(ul)
     for (let recipe of recipes) {
@@ -12,7 +14,68 @@ function displayRecipes(recipes) {
     }
 }
 
-function getRecipeList() {
+function printString(info) {
+    let section = document.getElementById("one-recipe");
+    let para = document.createElement("p");
+    section.appendChild(para);
+    para.textContent = info;
+}
+
+function printIngredients(info) {
+    let section = document.getElementById("one-recipe");
+    let ingredients = document.createElement("ul");
+    section.appendChild(ingredients);
+    console.log(`info: ${info}`);
+    ingredients.textContent = "Ingredients:";
+    for(let i = 0; i < info.length; i++) {
+        let ul = document.createElement("ul");
+        section.appendChild(ul);
+        ul.textContent = `${info[i].name}: ${info[i].quantity}`;
+    }
+}
+
+function printArray(key, value) {
+    let section = document.getElementById("one-recipe");
+    let keyName = document.createElement("ul");
+    section.appendChild(keyName);
+    keyName.textContent = `${key}: `;
+    for(let i = 0; i < value.length; i++) {
+        let ul = document.createElement("ul");
+        section.appendChild(ul);
+        ul.textContent = value[i];
+    }
+}
+
+function displayOneRecipe(recipe) {
+    let section = document.getElementById("one-recipe");
+    section.innerHTML = null;
+    printString(`Name: ${recipe.name}`);
+    printIngredients(recipe.ingredients)
+    printArray("Steps", recipe.steps);
+    printArray("Timer", recipe.timers);
+    printString(`Image: ${recipe.imageURL}`);
+    printString(`Original url: ${recipe.originalURL}`);
+}
+
+function getOneRecipe(event) {
+    fetch(recipeUrl)
+        .then(response => {
+            if (response.status !== 200) {
+                console.log("Got an error from the server:", response.status)
+                return
+            }
+            console.log("Got response from server:", response.body)
+            // Parsing the response
+            response.json().then(recipe => {
+                event.preventDefault();
+                console.log("parsed body", recipe[0])
+                displayOneRecipe(recipe[0]);
+            })
+        })
+        .catch(err => console.log(err))
+}
+
+function getRecipeList(event) {
     fetch(recipesUrl)
         .then(response => {
             if (response.status !== 200) {
@@ -22,11 +85,14 @@ function getRecipeList() {
             console.log("Got response from server:", response.body)
             // Parsing the response
             response.json().then(recipes => {
-                displayRecipes(recipes)
+                event.preventDefault();
+                displayRecipes(recipes);
                 console.log("parsed body", recipes)
             })
         })
         .catch(err => console.log(err))
 }
 
-getRecipeList()
+document.getElementById("getAllRecipes").addEventListener("click", getRecipeList);
+
+document.getElementById("getOneRecipe").addEventListener("click", getOneRecipe);
